7b23e2a954a65e6a7c4782a6b16e5d4a34099544
Bhavesh7755 <bhaveshvirom7755@gmail.com>
2025-09-14 18:14:47 +0530

    Implement user authentication methods: login, logout, password change, and token refresh

--- Backend/src/controllers/user-controller.js
+++ Backend/src/controllers/user-controller.js
@@ -4,6 +4,24 @@ import { User } from "../models/user.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+// generating the access token and refresh token method for login
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken; // save the refresh token in the user document or database
+        await user.save({ validateBeforeSave: false }); // save the user document with the new refresh token
+
+        return { accessToken, refreshToken };
+
+    } catch (erro) {
+        throw new ApiError(500, "Error Something while  generating tokens");
+    }
+}
 
 const registerUser = asyncHandler(async (req, res) => {
     // get user details from fronted
@@ -60,7 +78,7 @@ const registerUser = asyncHandler(async (req, res) => {
     const createUser = await User.findById(user._id).select("-password -refreshToken");
 
     // check for user creation
-    if(!createUser){
+    if (!createUser) {
         throw new ApiError(500, "User registration failed");
     }
 
@@ -69,6 +87,237 @@ const registerUser = asyncHandler(async (req, res) => {
     );
 });
 
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, username, password } = req.body;
+
+    if (!username && !email) {
+        throw new ApiError(400, "Username or email is required to login");
+    }
+
+    const user = await User.findOne({
+        $or: [{ email }, { username }]
+    })
+
+    if (!user) {
+        throw new ApiError(400, "User not found with this username or email");
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if (!isPasswordValid) {
+        throw new ApiError(400, "Invalid password");
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id);
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
+
+    const options = {
+        httpOnly: true,
+        secure: true, // only send cookie over https
+    }
+
+    return res.status(200).cookie("accessToken", accessToken, options).json(
+        new ApiResponse(
+            200, {
+            user: loggedInUser,
+            accessToken,
+            refreshToken
+        },
+            "User logged in successfully"
+        )
+    )
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined // remove the refresh token from the user document
+            }
+        },
+        {
+            new: true  // return the updated user document
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true, // set to true if using https
+    }
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", "", options) // clear the access token cookie
+        .clearCookie("refreshToken", "", options) // clear the refresh token cookie
+        .json(new ApiResponse(200, {}, "User logged out successfully"));
+})
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+    // 1. Get old and new Password from fronted side
+    // 2. get the user by req.user.id
+    // 3. check if old password is correct
+    // 4. hash the new password
+    // 5. update new password in user doucment
+    // 6. save the user
+    // 7. send response
+
+    // 1. Get old and new Password from fronted side
+    const { oldPassword, newPassword } = req.body;
+
+    // 2. get the user by req.user.id
+    const user = await User.findById(req.user?._id);
+
+    // 3. check if old password is correct
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Old password is incorrect");
+    }
+
+    // 4. hash the new password
+    user.password = newPassword;
+
+    // 5. update new password in user doucment
+    await user.save({ validateBeforeSave: false }); //6. save user and validation false for not to check other fields
+
+    // 7. send response
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Password changed successfully"));
+});
+
+// Method for generaing access token from refresh token after expiry of access token each time
+const refreshAccessToken = asyncHandler(async (req, res) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken; // get the refresh token from the cookie or request body from user side
+
+    if (!incomingRefreshToken) {
+        throw new ApiError(401, "No refresh token provided, please login first or Unauthorized request");
+    }
+
+    // verify the refresh token
+    const decodedToken = jwt.verify(
+        incomingRefreshToken,
+        process.env.REFRESH_TOKEN_SECRET
+    )
+
+    const user = await User.findById(decodedToken?._id)
+
+    if (!user) {
+        throw new ApiError(401, "Invalid refresh token or user does not exist");
+    }
+
+
+    if (incomingRefreshToken !== user.refreshToken) {
+        throw new ApiError(401, "Refresh token is expired or used");
+    }
+
+
+    try {
+        const options = {
+            httpOnly: true,
+            secure: true, // set to true if using https
+        }
+
+        const { newAccessToken, newRefreshToken } = await generateAccessToken(user._id);
+
+        return res
+            .status(200)
+            .cookie("accessToken", newAccessToken, options)
+            .cookie("refreshToken", newRefreshToken, options)
+            .json(
+                new ApiResponse(
+                    200,
+                    {
+                        accessToken: newAccessToken, refreshToken: newRefreshToken
+                    },
+                    "Access token refreshed successfully"
+                )
+            )
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid refresh token");
+    }
+})
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+    return res
+        .status(200)
+        .json(200, req.user, "Current user fetched successfully"); // req.user is coming from the verifyJWT middleware so that we have to use that middleware in the route and we can access the req.user
+})
+
+const updateAccountDetails = asyncHandler(async (req, res) => {
+    // 1. get user details from req.body
+    const { fullName, email, AddressLine1, AddressLine2, state, pincode, city } = req.body;
+
+    // 2. check if user details are not empty
+    if ([fullName, email, city, state, pincode, AddressLine1, AddressLine2].some((field) => field?.trim() === "")) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    // 3. check if user is exist and update details
+    const user = await User.findByIdAndUpdate(
+        req, user?._id,
+        {
+            $set: {
+                fullName: fullName,
+                email: email,
+                AddressLine1: AddressLine1,
+                AddressLine2: AddressLine2,
+                state: state,
+                pincode: pincode,
+                city: city,
+            }
+        },
+        { new: true } // return the updated user documnet
+    ).select("-password")
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, user, "Account details updated successfully"));
+})
+
+const updateUserAvatar = asyncHandler(async (req, res) => {
+    // 1. get the file from client/fronted
+    const avatarLocalPath = req.file?.path;
+
+    // 2. check if file is provided
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is missing");
+    }
+
+    // 3. upload the file to cloudinary
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+    // 4. check if avatar is uploaded ?
+    if (!avatar.url) {
+        throw new ApiError(500, "Error while uploading avatar on cloudinary");
+    }
+
+    // 5. update the user documnet
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url
+            }
+        },
+        { new: true }
+    ).select("-password");
+
+    // 6. send response
+    return res
+        .status(200)
+        .json(new ApiResponse(200, user, "User avatar updated successfully"));
+})
+
 export {
     registerUser,
+    loginUser,
+    logoutUser,
+    changeCurrentPassword,
+    refreshAccessToken,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
 };
--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { upload } from "../middlewares/multer.middleware.js";
-import { registerUser } from '../controllers/user-controller.js';
+import { changeCurrentPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar } from '../controllers/user-controller.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 
 const router = Router();
@@ -9,5 +10,13 @@ router.route("/register").post(
     upload.fields([{ name: 'avatar', maxCount: 1}]),
     registerUser
 );
+
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 
 export default router;
